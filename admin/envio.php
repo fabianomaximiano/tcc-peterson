4d013039fdf6fde88450135cf22b60dc684752ed
Fabiano Maximiano <fabianomaximiano@hotmail.com>
2025-02-27 11:51:43 -0300

    inclusão campo mensagem no formulario de cadastro

--- admin/envio.php
+++ admin/envio.php
@@ -1,16 +1,24 @@
 <?php 
 
+include  '../templates-parts/header-admin.php'; ?>
+
+<div class="container">
+
+<?php
+echo "<h1 class='h1'>Resultado do formulario de cadastro</h1>";
 $nome = $_POST["nome"];
 $sobrenome = $_POST["sobrenome"];
 $email = $_POST["email"];
 $dob = $_POST["dob"];
 $address = $_POST["address"];
+$message = $_POST["message"];
 echo "Meu nome é " . $nome . " sobrenome " . $sobrenome . ".</br>";
 echo "Email de contato: " . $email . ".</br>";
 echo "Data: " . $dob . ".</br>";
-echo "Moro neste endereço: " . $address . ".</br>";
-exit;
+echo "Moro neste endereço: " . $address . ".</br><hr>";
+echo "Mensagem: " . $message . ".</br></br>";
+exit; ?>
 
-
-
-?>
+</div>
+<?php
+   include  '../templates-parts/footer-admin.php'; ?>
